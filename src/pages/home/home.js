c77972b25cc715bbe2a41b2c5f817e6e1758686e
Волков Вадим <volkovva.pir231@gamil.com>
2025-09-07 22:53:24 +0300

    feat: add header.js file with header component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from '../../components/header/header.js'; 
-
+import Footer from '../../components/footer/footer.js';
 class Home extends React.Component{
     render () {
         return (
             <div>
                 <Header /> 
+                <Footer />
             </div>
         )
     }
