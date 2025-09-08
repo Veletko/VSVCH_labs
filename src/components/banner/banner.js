a6818f1901be18dd72ff461518263a516eae5b80
Волков Вадим <volkovva.pir231@gamil.com>
2025-09-08 11:54:32 +0300

    feat: add banner.js file wiht banner component

---
+++ src/components/banner/banner.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../../pages/home/home.css'
+import Exclamation_mark from '../../img/exclamation_mark.png'
+import Banner_woman from '../../img/banner-woman.jpg'
+
+class Banner extends React.Component {
+    render () {
+        return (
+            <section class="banner-container">
+            <div class="banner-container-left">
+                <div class="banner-container-text">
+                    <div class="banner-container-text-logo">
+                        <img src={Exclamation_mark} alt="!"/>
+                        <h3 data-i18n="homepage.banner.welcome">Welcome to Beautyness!!!</h3>
+                    </div>
+                    <h1 data-i18n="homepage.banner.title">Beauty is power a smile is its sword.</h1>
+                    <p data-i18n="homepage.banner.description">There are many variation of passages are
+                        Ipsum available, majority have suffered
+                        alteration in some form.</p>
+                </div>
+            </div>
+            <div class="banner-container-right">
+                <div class="image-frame">
+                    <img src={Banner_woman} alt="Spa Woman"/>
+                </div>
+            </div>
+        </section>
+        )
+    }
+}
+
+export default Banner
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../../components/header/header.js'; 
 import Footer from '../../components/footer/footer.js';
+import Banner from '../../components/banner/banner.js';
+
 class Home extends React.Component{
     render () {
         return (
             <div>
                 <Header /> 
+                <Banner />
                 <Footer />
             </div>
         )
