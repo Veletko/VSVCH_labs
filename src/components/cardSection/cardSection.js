14a847f8eefcede4194612e5689ed39025341825
Волков Вадим <volkovva.pir231@gamil.com>
2025-09-22 11:34:19 +0300

    refactor: rewrite cardsection to functional component

--- src/components/cardSection/cardSection.js
+++ src/components/cardSection/cardSection.js
@@ -2,19 +2,20 @@ import React from 'react';
 import '../../pages/home/home.css'
 import Card from '../card/card';
 
-class CardSection extends React.Component {
-
-    render(){
-        const {cardInfo} = this.props
-        return(
-             <section class="cards-container">
-                {cardInfo.map((item, index) => (
-                    <Card key={index} title={item.title} description={item.description}/>
-                )
-                ) }
-             </section>
-        )
-    }
+const CardSection = ({cardInfo, updateCard}) => {
+    return(
+        <section class="cards-container">
+            {cardInfo.map((item) => (
+                <Card 
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                image ={item.image}
+                updateCard = {updateCard}/>
+            ))}
+        </section>
+    )
 }
+
 
 export default CardSection
