6813277d80ae27acff804151f65226a8f6c87e64
Волков Вадим <volkovva.pir231@gamil.com>
2025-10-05 17:41:40 +0300

    refactor: migrate Card component to Material-UI

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,34 +1,61 @@
 import React, { useState } from 'react';
-import '../../pages/home/home.css'
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  Box
+} from '@mui/material';
 import Modal from '../modal/modal';
 
-const Card = ({id, title, description, image, updateCard}) =>{
+const ServiceCard = ({ id, title, description, image, updateCard }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false)
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
 
-    const handleOpenModal =  () => setIsModalOpen(true)
-    const handleCloseModal =  () => setIsModalOpen(false)
+  return (
+    <Box>
+      <Card>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={title}
+        />
+        
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h3">
+            {title}
+          </Typography>
+          
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        
+        <CardActions>
+          <Button size="small">Read More</Button>
+          <Button size="small" onClick={handleOpenModal}>
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
 
-    return ( 
-        <div className="card">
-            <img src={image} alt="service"/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <button className="card-button" data-i18n="homepage.services.button">Read More</button>
-            <button className='card-button' onClick={handleOpenModal}>Edit</button>
+      {isModalOpen && (
+        <Modal 
+          id={id}
+          title={title}
+          description={description}
+          image={image}
+          updateCard={updateCard}
+          onClose={handleCloseModal}
+        />
+      )}
+    </Box>
+  );
+};
 
-            {isModalOpen && (
-                <Modal 
-                id={id}
-                title= {title}
-                description={description}
-                image={image}
-                updateCard={updateCard}
-                onClose={handleCloseModal}/>
-            )}
-        </div>
-    )
-    
-}
-
-export default Card
+export default ServiceCard;
