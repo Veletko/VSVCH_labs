a051cf29049ba34d31c54a9929579d60fba12982
Волков Вадим <volkovva.pir231@gamil.com>
2025-10-05 20:41:56 +0300

    feat: add fuctionality to update cards info, and take cards info from json server

--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import CardSection from '../../components/cardSection/cardSection';
 
+const Services = () => {
+  const [cardInfo, setCardInfo] = useState([]);
 
-class Services extends React.Component {
-    render(){
-        const cardInfo = [
-            {title: "Spa & Massage", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."},
-            {title: "Hair & Beauty", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."},
-            {title: "Body Treatments", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."},
-            {title: "Spa & Massage", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."},
-            {title: "Hair & Beauty", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."},
-            {title: "Body Treatments", description: "If you are going to use a passage offer Lorem Ipsum, you need to be sure hidden in the middle of text."}
-        ]
-        return(
-            <div>
-                <Header/>
-                <CardSection  cardInfo={cardInfo}/>
-                <Footer/>
-            </div>
-        )
-    }
-}
-export default Services
+
+  useEffect(() => {
+    fetch('http://localhost:3000/cards')
+      .then((response) => response.json())
+      .then((data) => setCardInfo(data))
+      .catch((error) => console.error('Ошибка при загрузке данных:', error));
+  }, []); 
+
+  const updateCard = (updatedCard) => {
+    setCardInfo((previous) =>
+      previous.map((card) => (card.id === updatedCard.id ? updatedCard : card))
+    );
+    console.log('Обновление карточки с ID:', updatedCard.id);
+    fetch(`http://localhost:3000/cards/${updatedCard.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedCard),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error('Ошибка при обновлении карточки:', error));
+  };
+
+  return (
+    <div>
+      <Header />
+      <CardSection cardInfo={cardInfo} updateCard={updateCard} />
+      <Footer />
+    </div>
+  );
+};
+
+export default Services;
